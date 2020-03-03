00c92c32a7120e9f14ec878b32a1a3341e81bd55
Filipe Mazzon <filipe.mazzon@olhonocarro.com.br>
2020-03-03 00:32:16 -0300

    :bug: fix method castMapToArrayOfObject

--- parser/helpers/game.helper.js
+++ parser/helpers/game.helper.js
@@ -1,19 +1,19 @@
 const WORLD = '<world>';
 
 const castMapToArrayOfObject = (map) => {
-    return Array.from(map).map((key, value) => ({
+    return Array.from(map).map(([key]) => ({
         player: key,
-        kills: value
+        kills: map.get(key)
     }))
 };
 
 const isNotSuicide = (playerKiller, playerKilled) => {
-    return (playerKiller === playerKilled)
+    return (playerKiller !== playerKilled)
 };
 
 const wasNotWorldWhoKilled = (playerKiller) => {
-    return (playerKiller === WORLD)
-}
+    return (playerKiller !== WORLD)
+};
 
 module.exports = {
     castMapToArrayOfObject,
